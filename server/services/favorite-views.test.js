a3e20bbb4471f03d8aae5854f81c995f245a54f9
Benoît Vidis <bvidis@kaliop.com>
2024-05-29 15:10:04 +0200

    feat: init tests

---
+++ server/services/favorite-views.test.js
@@ -0,0 +1,115 @@
+const createFavoriteViews = require('./favorite-views');
+
+describe('services/favorite-views', () => {
+  let fav;
+  let strapi;
+  let user;
+
+  beforeEach(() => {
+    strapi = {
+      admin: {
+        config: {
+          url: 'strapi.admin.config.url'
+        }
+      },
+      entityService: {
+        count: jest.fn(async () => 42),
+        create: jest.fn(async () => {}),
+        delete: jest.fn(async () => {}),
+        findMany: jest.fn(async () => [{ foo: 'bar' }]),
+        update: jest.fn(async () => {})
+      }
+    };
+    user = {
+      id: 'userId',
+      roles: ['role1', 'role2']
+    };
+
+    fav = createFavoriteViews({ strapi });
+  });
+
+  test('create', async () => {
+    const name = 'name';
+    const slug = 'slug';
+    const roles = ['role1', 'role2'];
+    const visibility = 'visibility';
+    const userId = 'userId';
+
+    await fav.create(name, slug, roles, visibility, userId);
+    const [uid, data] = strapi.entityService.create.mock.calls[0];
+
+    expect(uid).toBe('plugin::favorite-views.saved-view');
+    expect(data).toStrictEqual({
+      data: {
+        name: 'name',
+        slug: 'slug',
+        roles: ['role1', 'role2'],
+        visibility: 'visibility',
+        createdBy: 'userId'
+      }
+    });
+  });
+
+  test('delete', async () => {
+    await fav.delete('id');
+
+    const [, id] = strapi.entityService.delete.mock.calls[0];
+    expect(id).toBe('id');
+  });
+
+  test('getPrivateViews', async () => {
+    const response = await fav.getPrivateViews(user);
+    expect(response).toStrictEqual({
+      privateViewsData: [{ foo: 'bar' }]
+    });
+  });
+
+  test('getSharedViews', async () => {
+    const response = await fav.getSharedViews({}, user);
+
+    expect(response).toStrictEqual({
+      sharedViewsData: [{ foo: 'bar' }],
+      pagination: { currentPage: 1, pageSize: 10, totalPages: 5, count: 42 }
+    });
+  });
+
+  test('getUserViews', async () => {
+    const response = await fav.getUserViews(
+      {
+        start: 6,
+        limit: 12
+      },
+      user
+    );
+
+    expect(strapi.entityService.findMany.mock.calls[0][0]).toBe(
+      'plugin::favorite-views.saved-view'
+    );
+
+    expect(response).toStrictEqual({
+      userViewsData: [{ foo: 'bar' }],
+      pagination: { currentPage: 1, pageSize: 10, totalPages: 5, count: 42 }
+    });
+  });
+
+  test('update', async () => {
+    const foobar = { foo: 'bar' };
+    strapi.entityService.update = jest.fn(async () => foobar);
+
+    expect(await fav.update('id', 'name', ['role1', 'role2'], 'visibility', 'userId')).toBe(foobar);
+
+    const [uid, id, data] = strapi.entityService.update.mock.calls[0];
+
+    expect(uid).toBe('plugin::favorite-views.saved-view');
+    expect(id).toBe('id');
+
+    expect(data).toStrictEqual({
+      data: {
+        name: 'name',
+        roles: ['role1', 'role2'],
+        visibility: 'visibility',
+        updatedBy: 'userId'
+      }
+    });
+  });
+});
